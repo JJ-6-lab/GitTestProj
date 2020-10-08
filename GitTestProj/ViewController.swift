b6b69d8949675178999a3c9a3d15a86fcad46b51
nofprog <k.jin3217@gmail.com>
2020-10-08 14:38:06 +0900

    Initial Commit

---
+++ GitTestProj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTestProj
+//
+//  Created by J on 2020/10/08.
+//  Copyright © 2020 nof. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
